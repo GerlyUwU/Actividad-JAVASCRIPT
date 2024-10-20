dc6ac885183ad8ef9c114272e741a87a474820f6
Gerly Daniel Arteaga Bernal <arteaga.bernal3@gmail.com>
2024-10-20 05:33:53 -0600

    iniciacion, ejercicio 4 resuelto

---
+++ iniciacion/Ejercicio4/ejercicio4.js
@@ -0,0 +1,8 @@
+const a = "Hola";
+let b = " mundo!";
+
+document.write("Constante a contiene " + a );
+document.write("</br>");
+document.write("Variable b contiene " + b );
+document.write("</br>");
+document.write(a + b );
